ff17299a14962c2fda2e70fa26f4b6f330bb5f2b
Arakelyan Artak <44243461+artakarakelyan27@users.noreply.github.com>
2025-07-07 08:20:22 +0300

    Delete todolist directory

--- todolist/config/database.php
+++
@@ -1,7 +0,0 @@
-<?php
-define('DB_HOST', 'localhost');
-define('DB_USER', 'todo');
-define('DB_PASS', 'gfhjkmjntodo');
-define('DB_NAME', 'todo_bd');
-
-?>
--- todolist/public/footer.php
+++
@@ -1,4 +0,0 @@
-<script src="script/jquery.js"></script>
-<script src="script/app.js"></script>
-</body>
-</html>
